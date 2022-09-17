72d3619bbd541349fb1548922cf58452a3a4bc07
SortaHappyDoge <113366609+SortaHappyDoge@users.noreply.github.com>
2022-09-17 20:45:20 +0300

    Camera Rotation
    Added simple plain, playerbody and camera rotation.

---
+++ Valorant Fangame/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera;
+    [SerializeField] Transform playerBody;
+
+    public float mouseSens = 10f;
+    float mouseX;
+    float mouseY;
+    float xRotation;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void Update()
+    {
+        mouseX += Input.GetAxisRaw("Mouse X") * mouseSens;
+        mouseY = Input.GetAxisRaw("Mouse Y") * mouseSens;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        playerCamera.rotation = Quaternion.Euler(xRotation, mouseX, playerCamera.rotation.z);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
